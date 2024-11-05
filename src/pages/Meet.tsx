13a62f29f8a2afe5281dac48d1d9ba1cfc800164
Aivan Monceller <aivan@monceller.com>
2024-11-05 22:13:05 +0800

    fix textarea style

--- src/pages/Meet.tsx
+++ src/pages/Meet.tsx
@@ -263,7 +263,7 @@ function CubemeetForm({ onSubmit }: CubemeetFormProps) {
         <textarea
           id="contact"
           rows={3}
-          className="w-full rounded-md border-gray-300 hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:border-yellow-300 placeholder:text-gray-500"
+          className="w-full rounded-md border border-gray-300 bg-white px-3 py-2 hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:border-yellow-300 placeholder:text-gray-500"
           placeholder="Facebook name and contact number"
           required
         />
